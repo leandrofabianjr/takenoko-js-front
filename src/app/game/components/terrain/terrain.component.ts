f0794231b60a564e8b8c3a04b7d0a7ec24d85743
leandrofabianjr <leandrofabianjr@gmail.com>
2020-07-05 14:42:20 -0300

    Changes in terrain component layout

--- src/app/game/components/terrain/terrain.component.ts
+++ src/app/game/components/terrain/terrain.component.ts
@@ -12,6 +12,23 @@ export class TerrainComponent implements OnInit {
   @Input() slot: TerrainSlot;
   @Output() selected = new EventEmitter<{ x: number, y: number }>();
 
+  get terrainTypeClass(): string | string[] {
+    switch (true) {
+      case !this.slot?.terrain && this.slot?.canBeTerrain:
+        return 'can-be-terrain';
+      case this.slot?.terrain?.isFountain:
+        return 'fountain';
+      case this.slot?.terrain && !this.slot.terrain?.isFountain:
+        return this.slot?.terrain?.color ? ['terrain', this.slot?.terrain?.color] : 'terrain';
+      default:
+        return 'empty-terrain';
+    }
+  }
+
+  get isTerrain() {
+    return this.slot?.terrain && !this.slot.terrain?.isFountain;
+  }
+
   constructor() { }
 
   ngOnInit() {
